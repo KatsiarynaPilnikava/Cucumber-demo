27221a49d1afd5d84c010cf94ec02446c39f719c
Katsiaryna Pilnikava <Katsiaryna_Pilnikava@epam.com>
2017-06-13 12:52:04 +0300

    copy proj to the repo

---
+++ src/main/java/Cucumber_demo/Cucumber_demo/Belly.java
@@ -0,0 +1,23 @@
+package Cucumber_demo.Cucumber_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Belly 
+{
+
+    private int cukes;
+
+    public void eat(int cukes) {
+        this.cukes = cukes;
+    }
+
+    public String getSound(int waitingTime) {
+        if (cukes > 41 && waitingTime >= 1) {
+            return "growl";
+        } else {
+            return "silent";
+        }
+    }
+}
